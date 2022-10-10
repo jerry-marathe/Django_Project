540e51f5e296d2f65a6ee403d8fbae5ee22679a6
jerry-marathe <97115929+jerry-marathe@users.noreply.github.com>
2022-10-10 19:34:52 +0530

    Latest Update
    With Admin Site working

--- Winkelen_Stop/user_profile/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms  
-from user_profile.models import Profile 
-class ProfileForm(forms.ModelForm):  
-    class Meta:  
-        model = Profile  
-        fields = "__all__"  
